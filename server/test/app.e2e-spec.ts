dba2d2047ee5c183997d6b249d0b14bc681ab4f6
T. Zelinker <toviz@skyvar.co.il>
2024-10-09 16:26:29 +0300

    16:26

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,53 +1,20 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { INestApplication } from '@nestjs/common';
-// import * as request from 'supertest';
-// import { AppModule } from './../src/app.module';
-// import { MongooseModule } from '@nestjs/mongoose';
-
-// describe('AppController (e2e)', () => {
-//   let app: INestApplication;
-
-//   beforeEach(async () => {
-//     const moduleFixture: TestingModule = await Test.createTestingModule({
-//       imports: [AppModule],
-//     }).overrideProvider(MongooseModule)
-//       .useValue({}).compile();
-
-//     app = moduleFixture.createNestApplication();
-//     await app.init();
-//   });
-
-//   it('/ (GET)', () => {
-//     return request(app.getHttpServer())
-//       .get('/')
-//       .expect(200)
-//       .expect('Hello World!');
-//   });
-// });
-
-
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { closeInMongodConnection, rootMongooseTestModule } from '@server/utils/mongo'; // Import helper functions for in-memory MongoDB testing
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let moduleFixture: TestingModule;
 
-  beforeAll(async () => {
-    moduleFixture = await Test.createTestingModule({
-      imports: [rootMongooseTestModule(), AppModule],
-    }).compile();
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).overrideProvider(MongooseModule)
+      .useValue({}).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
-
-  afterAll(async () => {
-    await app.close();
-    await closeInMongodConnection(); // Close the in-memory MongoDB connection
   });
 
   it('/ (GET)', () => {
@@ -57,3 +24,36 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 });
+
+
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { INestApplication } from '@nestjs/common';
+// import { AppModule } from './../src/app.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { closeInMongodConnection, rootMongooseTestModule } from '@server/utils/mongo'; // Import helper functions for in-memory MongoDB testing
+
+// describe('AppController (e2e)', () => {
+//   let app: INestApplication;
+//   let moduleFixture: TestingModule;
+
+//   beforeAll(async () => {
+//     moduleFixture = await Test.createTestingModule({
+//       imports: [rootMongooseTestModule(), AppModule],
+//     }).compile();
+
+//     app = moduleFixture.createNestApplication();
+//     await app.init();
+//   });
+
+//   afterAll(async () => {
+//     await app.close();
+//     await closeInMongodConnection(); // Close the in-memory MongoDB connection
+//   });
+
+//   it('/ (GET)', () => {
+//     return request(app.getHttpServer())
+//       .get('/')
+//       .expect(200)
+//       .expect('Hello World!');
+//   });
+// });
