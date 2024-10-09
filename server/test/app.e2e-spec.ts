50f1be7684b0b8f0efa1ad4ede83b6557ab31cb7
T. Zelinker <toviz@skyvar.co.il>
2024-10-09 16:23:45 +0300

    16:23

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,20 +1,53 @@
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { INestApplication } from '@nestjs/common';
+// import * as request from 'supertest';
+// import { AppModule } from './../src/app.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+
+// describe('AppController (e2e)', () => {
+//   let app: INestApplication;
+
+//   beforeEach(async () => {
+//     const moduleFixture: TestingModule = await Test.createTestingModule({
+//       imports: [AppModule],
+//     }).overrideProvider(MongooseModule)
+//       .useValue({}).compile();
+
+//     app = moduleFixture.createNestApplication();
+//     await app.init();
+//   });
+
+//   it('/ (GET)', () => {
+//     return request(app.getHttpServer())
+//       .get('/')
+//       .expect(200)
+//       .expect('Hello World!');
+//   });
+// });
+
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { closeInMongodConnection, rootMongooseTestModule } from '@server/utils/mongo'; // Import helper functions for in-memory MongoDB testing
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let moduleFixture: TestingModule;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).overrideProvider(MongooseModule)
-      .useValue({}).compile();
+  beforeAll(async () => {
+    moduleFixture = await Test.createTestingModule({
+      imports: [rootMongooseTestModule(), AppModule],
+    }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await closeInMongodConnection(); // Close the in-memory MongoDB connection
   });
 
   it('/ (GET)', () => {
