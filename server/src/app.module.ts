6209a482ac6dfbfa7943537549601623dba7b8b2
T. Zelinker <toviz@skyvar.co.il>
2024-10-10 10:36:10 +0300

    10:36

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,7 +7,7 @@ import { CategoryModule } from './category/category.module';
 @Module({
   imports: [process.env.MONGO_URI !== 'test'
   ? MongooseModule.forRoot(process.env.MONGO_URI)
-  : MongooseModule.forRoot('mongodb://localhost:27017/test', { useNewUrlParser: true }),
+  : MongooseModule.forRoot('mongodb://localhost/test'),
 CategoryModule,],
   controllers: [AppController],
   providers: [AppService],
