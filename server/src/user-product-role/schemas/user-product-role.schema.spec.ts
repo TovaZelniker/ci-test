0b8c633fddfe0a7b9733d714066c66b380397e75
T. Zelinker <toviz@skyvar.co.il>
2024-12-11 18:42:50 +0200

    18:42

--- server/src/user-product-role/schemas/user-product-role.schema.spec.ts
+++ server/src/user-product-role/schemas/user-product-role.schema.spec.ts
@@ -18,10 +18,6 @@ afterAll(async () => {
   await mongoServer.stop();
 });
 
-beforeEach(async () => {
-  await UserProductRoleModel.deleteMany({});
-});
-
 describe('UserProductRole Schema Validations', () => {
   it('should create a valid UserProductRole document', () => {
     const data = {
