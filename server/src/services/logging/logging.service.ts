1a802df3db9725ec8f551f3f4300c900fcd6357c
T. Zelinker <toviz@skyvar.co.il>
2024-12-22 09:58:43 +0200

    09:58

--- server/src/services/logging/logging.service.ts
+++ server/src/services/logging/logging.service.ts
@@ -5,11 +5,11 @@ import * as constants from '../../utils/constants';
 
 @Injectable()
 export class LoggingService {
-  private logFile = fs.createWriteStream(constants.PATH_LOGGING, { flags: 'a' });
   writeToLog(error: string): void {
+    const logFile = fs.createWriteStream(constants.PATH_LOGGING, { flags: 'a' });
     const date = new Date();
-    this.logFile.write(`${date}\n`);
-    this.logFile.write(`${util.format(error)}\n\n`);
+    logFile.write(`${date}\n`);
+    logFile.write(`${util.format(error)}\n\n`);
   }
 
 }
